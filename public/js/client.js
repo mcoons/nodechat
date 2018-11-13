02f4fd98c1cb0d4527c423535f21d59e077ff4bd
mcoons <mcoons67@gmail.com>
2018-11-13 13:58:01 -0700

    cleaned code

--- public/js/client.js
+++ public/js/client.js
@@ -1,11 +1,13 @@
 
 $(function () {
-  
   var username = prompt("Please enter your chat name:", "Unknown");
 
-
   var socket = io();
-  // socket.emit('user joined to server', username);
+  
+  // send join message to server
+  socket.on("connect", function(){
+    socket.emit('user joined to server', username);
+  })
 
   // send chat message to server after button click
   $('form').submit(function(){
@@ -17,24 +19,8 @@ $(function () {
   // receive chat message from server
   socket.on('chat message', function(msg){
     $('#messages').append($('<li>').text(msg));
-
-    // $('#messages').scrollTop($('#messages')[0].scrollHeight)
-
-    // chatWindow = document.getElementById('messages'); 
-    // var xH = chatWindow.scrollHeight; 
-    // chatWindow.scrollTo(0, xH);
-
     window.scrollTo(0,document.body.scrollHeight);
+    // $('#messages').scrollTop($('#messages')[0].scrollHeight)
   });
- 
-
-  socket.on("connect", function(){
-    socket.emit('user joined to server', username);
-  })
-
-  // socket.on('disconnect', function(username){
-  //   socket.emit('user left', username);
-  // })
-
 
 });
